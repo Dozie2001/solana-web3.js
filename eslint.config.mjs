e9d6a494289e41da7af6ebdf2206a75be1560f1d
Dozie2001 <davidchidozie402@gmail.com>
2024-11-17 14:07:29 +0100

    Enabled no-unsafe-enum-comparison

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ export default [
             '@typescript-eslint/no-unsafe-argument': 'off',
             '@typescript-eslint/no-unsafe-assignment': 'off',
             '@typescript-eslint/no-unsafe-call': 'off',
-            '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+            '@typescript-eslint/no-unsafe-enum-comparison': 'on',
             '@typescript-eslint/no-unsafe-member-access': 'off',
             '@typescript-eslint/no-unsafe-return': 'off',
             '@typescript-eslint/only-throw-error': 'off',
